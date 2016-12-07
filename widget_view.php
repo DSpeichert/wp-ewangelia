74718ddd27e0e3ad22e1ca06a3faa6461a867b75
Daniel Speichert <daniel@speichert.pl>
2016-12-07 11:01:36 -0500

    Remove blackquote

--- widget_view.php
+++ widget_view.php
@@ -19,13 +19,11 @@ if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?
     <p>
         <em><?php echo date_i18n(get_option('date_format'), strtotime($post->post_date)) ?></em><br/>
         <a href="<?php the_permalink(); ?>" rel="bookmark" title="Link do <?php the_title_attribute(); ?>"><?php the_title(); ?></a><br/>
-        <blockquote>
-            <?php echo get_the_excerpt();
-            if (array_key_exists('link_page', $instance) && !empty($instance['link_page'])) {
-                echo ' <a href="' . get_permalink($instance['link_page']) . '">Czytaj całość</a>';
-            }
-            ?>
-        </blockquote>
+        <?php echo get_the_excerpt();
+        if (array_key_exists('link_page', $instance) && !empty($instance['link_page'])) {
+            echo ' <a href="' . get_permalink($instance['link_page']) . '">Czytaj całość</a>';
+        }
+        ?>
     </p>
 <?php endwhile;
     wp_reset_postdata();
